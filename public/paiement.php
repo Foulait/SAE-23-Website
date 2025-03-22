ab02f000100d1da4f18c5531fcf639a3cadfad20
Foulait <trigorhjiovannni@gmail.com>
2025-03-22 16:31:12 +0100

    algo the luhn à intégrer à la page de paiement

---
+++ public/paiement.php
@@ -0,0 +1,17 @@
+// fonction utilisée pour vérifier les codes de cartes bancaire il faut l'intégrer à la page de paiement.
+
+function luhnValidation($numero) {
+    $sum = 0;
+    $len = strlen($numero);
+    for ($i = $len - 1; $i >= 0; $i--) {
+        $digit = $numero[$i];
+        if ($i % 2 == 1) {
+            $digit = $digit * 2;
+            if ($digit > 9) {
+                $digit -= 9;
+            }
+        }
+        $sum += $digit;
+    }
+    return $sum % 10 === 0;
+}
